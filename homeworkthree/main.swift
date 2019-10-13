97cf50f2c282c9d8dd74097870e14917aadb5125
Nikita Boiko <genertyniki@gmail.com>
2019-10-13 20:42:57 +0300

    Initial Commit

---
+++ homeworkthree/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  homeworkthree
+//
+//  Created by Nikita Boiko on 13/10/2019.
+//  Copyright © 2019 Nikita Boiko. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
