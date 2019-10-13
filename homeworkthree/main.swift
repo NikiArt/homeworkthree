ec424f174d81119655a9d5e5f96fb2ad7e9b469f
Nikita Boiko <genertyniki@gmail.com>
2019-10-13 21:47:45 +0300

    all tasks done

--- homeworkthree/main.swift
+++ homeworkthree/main.swift
@@ -8,21 +8,127 @@
 
 import Foundation
 
+enum CarActions: String {
+    case startEngine = "Запуск двигателя"
+    case stopEngine = "Остановка двигателя"
+    case openWindows = "Открытие окон"
+    case closeWindows = "Закрытие окон"
+    case getInTrunk = "Загрузка кузова"
+    case getOffTrunk = "Разгрузка кузова"
+}
+
 struct Car {
     var brand: String
     var year: Int
-    var volume: Int
+    var volume: UInt
     var isStartEngine: Bool
     var isOpenWindows: Bool
-    var currentVolume: Int
+    var currentVolume: UInt
+    
+    init(brand: String, year: Int, volume: UInt) {
+        self.brand = brand
+        self.year = year
+        self.volume = volume
+        isOpenWindows = false
+        isStartEngine = false
+        currentVolume = 0
+    }
+    
+    mutating func action(actionType: CarActions, volume: UInt = 0) {
+        switch actionType {
+        case .startEngine:
+            isStartEngine = true
+            print("У автомобиля \(brand): Двигатель запущен")
+        case .stopEngine:
+            isStartEngine = false
+            print("У автомобиля \(brand): Двигатель заглушен")
+        case .openWindows:
+            isOpenWindows = true
+            print("У автомобиля \(brand): Окна открыты")
+        case .closeWindows:
+            isOpenWindows = false
+            print("У автомобиля \(brand): Окна закрыты")
+        case .getInTrunk:
+            let amount = volume + currentVolume
+            if amount > self.volume {
+                currentVolume = self.volume
+                print("У автомобиля \(brand): Превышен допустимый объем. \(amount - self.volume) осталось не загружено")
+            } else {
+                currentVolume += volume
+            }
+            print("У автомобиля \(brand): Текущая загрузка составляет \(currentVolume)")
+        case .getOffTrunk:
+            let amount = Int(currentVolume - volume)
+            if amount < 0 {
+                currentVolume = 0
+                print("У автомобиля \(brand): Автомобиль разгружен полностью.")
+            } else {
+                currentVolume -= volume
+            }
+            print("У автомобиля \(brand): Текущая загрузка составляет \(currentVolume)")
+        }
+    }
 }
 
 struct Truck {
     var brand: String
     var year: Int
-    var volume: Int
+    var volume: UInt
     var isStartEngine: Bool
     var isOpenWindows: Bool
-    var currentVolume: Int
+    var currentVolume: UInt
+    
+    init(brand: String, year: Int, volume: UInt) {
+        self.brand = brand
+        self.year = year
+        self.volume = volume
+        isOpenWindows = false
+        isStartEngine = false
+        currentVolume = 0
+    }
+    
+    mutating func action(actionType: CarActions, volume: UInt = 0) {
+        switch actionType {
+        case .startEngine:
+            isStartEngine = true
+            print("У грузовика \(brand): Двигатель запущен")
+        case .stopEngine:
+            isStartEngine = false
+            print("У грузовика \(brand): Двигатель заглушен")
+        case .openWindows:
+            isOpenWindows = true
+            print("У грузовика \(brand): Окна открыты")
+        case .closeWindows:
+            isOpenWindows = false
+            print("У грузовика \(brand): Окна закрыты")
+        case .getInTrunk:
+            let amount = volume + currentVolume
+            if amount > self.volume {
+                currentVolume = self.volume
+                print("У грузовика \(brand): Превышен допустимый объем. \(amount - self.volume) осталось не загружено")
+            } else {
+                currentVolume += volume
+            }
+            print("У грузовика \(brand): Текущая загрузка составляет \(currentVolume)")
+        case .getOffTrunk:
+            let amount = Int(currentVolume - volume)
+            if amount < 0 {
+                currentVolume = 0
+                print("У грузовика \(brand): Автомобиль разгружен полностью.")
+            } else {
+                currentVolume -= volume
+            }
+            print("У грузовика \(brand): Текущая загрузка составляет \(currentVolume)")
+        }
+    }
 }
+
+var carOne = Car(brand: "Honda", year: 2012, volume: 830)
+var carTwo = Car(brand: "Nissan", year: 2008, volume: 1300)
+var truckOne = Truck(brand: "SCANIA", year: 2012, volume: 3500)
+carOne.action(actionType: .startEngine)
+carTwo.action(actionType: .getInTrunk, volume: 800)
+carTwo.action(actionType: .openWindows)
+carTwo.action(actionType: .getInTrunk, volume: 800)
+truckOne.action(actionType: .startEngine)
 
